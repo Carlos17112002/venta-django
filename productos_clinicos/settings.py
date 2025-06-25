c5e6c158cbb2159457f8aeaa4efffa957913261c
Carlos <ceplazacastillo@gmail.com>
2025-06-25 18:39:23 -0400

    se supone que se arreglo lo del carruel

--- productos_clinicos/settings.py
+++ productos_clinicos/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
@@ -121,7 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
@@ -139,5 +140,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'  # o una ruta absoluta adecuada
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  # o usa os.path.join(BASE_DIR, "static") si usas os
+]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
