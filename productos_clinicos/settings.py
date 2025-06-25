18edfffb41164d04d6aca4e95b1ed87357436790
Carlos <ceplazacastillo@gmail.com>
2025-06-25 18:11:55 -0400

    se supone que se arreglo lo del carruel

--- productos_clinicos/settings.py
+++ productos_clinicos/settings.py
@@ -135,3 +135,7 @@ LOGIN_URL = '/login/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'  # o una ruta absoluta adecuada
+
